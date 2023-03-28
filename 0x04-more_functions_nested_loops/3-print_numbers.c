6b6e705cf5c75900c67c7aa5e2d5868c022a67b6
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-03-28 21:10:13 +0000

    Add print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - print out from 0 to 9
+* Return: Alaways (0)
+*/
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(48 + i);
+	}
+	
+	_putchar(10);
+}
