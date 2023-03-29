01d800b33617e6b281440227f7557ae64b40e11b
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-03-29 15:01:20 +0000

    Add largets prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - get the largest prime number
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long prime = 612852475143;
+	long maxPrime = 2;	
+
+	while (maxPrime <= (prime / 2))
+	{
+		if (prime % maxPrime == 0)
+		{
+			prime /= maxPrime;
+		}
+		else
+			maxPrime++;
+	}
+
+	printf("%ld\n", maxPrime);
+
+	return (0);
+}
