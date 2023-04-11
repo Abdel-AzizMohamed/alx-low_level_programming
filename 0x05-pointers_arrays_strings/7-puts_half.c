45068cc27420b821f12bb5e25ddba79e4893452d
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-11 16:54:39 +0000

    Add puts half file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,23 @@ void puts_half(char *str)
 	int len, i;
 
 	while (str[len] != '\0')
+	{
 		len++;
-	
+	}
+
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; i < len; i++)
+		{
 			_putchar(str[i]);
+		}
 	}
 	else
 	{
 		for (i = (len - 1) / 2 + 1; i < len; i++)
+		{
 			_putchar(str[i]);
+		}
 	}
 
 	_putchar(10);
