52a46f574f6f172a648c4a6bc1076ead174aeb9b
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-12 10:44:55 +0000

    Add strncat file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates to string with a given max bytes
+ * @src: copied string
+ * @dest: destintion string
+ * @n: max bytes
+ * Return: concated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i, destLen;
+
+    destLen = 0;
+
+    while (dest[destLen] != '\0')
+        destLen++;
+
+    for (i = 0; i < n; i++)
+    {
+        if (src[i] == '\0')
+            break;
+        dest[destLen++] = src[i];
+    }
+
+    dest[destLen] = '\0';
+
+    return (dest);
+}
+
