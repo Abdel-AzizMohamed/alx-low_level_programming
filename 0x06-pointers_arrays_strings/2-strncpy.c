ad59976ec3b65164e5f72e5b7dc66bbfc5aee986
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-13 20:03:38 +0000

    Add strncpy file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates to string with a given max bytes
+ * @src: copied string
+ * @dest: destintion string
+ * @n: max bytes
+ * Return: concated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0' || dest[i] == '\0')
+			break;
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
