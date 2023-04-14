56ec50f72f65d385a9f407b05877769718beb3df
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-14 14:30:03 +0000

    Add rev array file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,18 +3,18 @@
 
 /**
  * reverse_array - reverse the order of array on integers
- * @a: the array 
+ * @a: the array
  * @n: number of elements in the array
- * Return: Nothing 
+ * Return: Nothing
  */
 void reverse_array(int *a, int n)
 {
 	int i, lastTmp;
 
-	for (i = 0; i < n/2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		lastTmp = a[n-(i+1)];
-		a[n-(i+1)] = a[i];
+		lastTmp = a[n - (i + 1)];
+		a[n - (i + 1)] = a[i];
 		a[i] = lastTmp;
 	}
 }
