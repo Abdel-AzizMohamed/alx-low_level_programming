0d24ae9208100376fc4379c428fbc3b4d270a21f
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-14 17:24:09 +0000

    Add leet file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encode string into 1337 
+ * @s: the string
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	unsigned int a, b, c;
+
+	char charList[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int intList[] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < sizeof(charList); b++)
+		{
+			if (charList[b] == s[a])
+			{
+				s[a] = intList[c];
+			}
+			c++;
+		}
+	}
+
+	return (s);
+}
