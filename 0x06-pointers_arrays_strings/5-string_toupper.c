6f3b5b287a3d710f79dcc2bdbf88488916621c69
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-14 14:43:51 +0000

    Add string toupper file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - converts string lowercase letters
+ * to uppercase 
+ * @s: the string
+ * Return: uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = (s[i] - 'a') + 'A';
+		i++;
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,34 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int a[] = {1,2,3,4,5};
+    char str[] = "Look up!\n";
+    char *ptr;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
     return (0);
 }
