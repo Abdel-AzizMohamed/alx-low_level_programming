a52a790d21d0b7ff3a19078c3adfd7245c4d6d35
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-15 11:16:01 +0000

    Add print numbers file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,67 +1,25 @@
 #include "main.h"
-#include <stdio.h>
-
 
 /**
- * base10 - give you the power of 10 n times
- * @n: power of 10
- * Return: 10 power of n
-*/
-int base10(int n)
-{
-	int base;
-
-	base = 10;
-
-	if (n == 0)
-		base = 1;
-
-	while (n > 1)
-	{
-		base *= 10;
-		n--;
-	}
-
-	return (base);
-
-}
-
-/**
- * print_number - prints out given numbers
- * @n: the number that will be printed
- * Return: Nothing
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
 void print_number(int n)
 {
-	int nLen, i, tmp, fullNum, resetNum, wNum;
+	unsigned int n1;
 
-	nLen = 0;
-	tmp = n;
-
-	if (n == 0)
-		_putchar(48);
-
-	else
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			n *= -1;
-			tmp *= -1;
-			_putchar('-');
-		}
-
-		while (tmp >= 1)
-		{
-			tmp /= 10;
-			nLen++;
-		}
-
-		for (i = 0; i < nLen; i++)
-		{
-			fullNum = n % base10(nLen - i);
-			resetNum = n % base10(nLen - (i + 1));
-			wNum = (fullNum - resetNum) / base10(nLen - i - 1);
-			_putchar(wNum + 48);
-		}
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
 	}
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
+
---
+++ 0x06-pointers_arrays_strings/101-print_number_backup.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * base10 - give you the power of 10 n times
+ * @n: power of 10
+ * Return: 10 power of n
+*/
+int base10(int n)
+{
+	int base;
+
+	base = 10;
+
+	if (n == 0)
+		base = 1;
+
+	while (n > 1)
+	{
+		base *= 10;
+		n--;
+	}
+
+	return (base);
+
+}
+
+/**
+ * print_number - prints out given numbers
+ * @n: the number that will be printed
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	int nLen, i, tmp, fullNum, resetNum, wNum;
+
+	nLen = 0;
+	tmp = n;
+
+	if (n == 0)
+		_putchar(48);
+
+	else
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			tmp *= -1;
+			_putchar('-');
+		}
+
+		while (tmp >= 1)
+		{
+			tmp /= 10;
+			nLen++;
+		}
+
+		for (i = 0; i < nLen; i++)
+		{
+			fullNum = n % base10(nLen - i);
+			resetNum = n % base10(nLen - (i + 1));
+			wNum = (fullNum - resetNum) / base10(nLen - i - 1);
+			_putchar(wNum + 48);
+		}
+	}
+}
