ffe1548d3ebec29e35aeb200d21c29796a95157f
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-15 09:46:12 +0000

    Add rot13 file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *s)
 	char c1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char c2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (a = 0; s[i] != '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
 		for (b = 0; c1[b] != '\0'; b++)
 		{
