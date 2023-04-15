8f7eb1594ccaf887b3a609dbc34e12b263ed0a96
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-15 21:22:20 +0000

    Fix betty for what my name file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 
 /**
  * main - print out file name
+ * @argc: number of params in command line
+ * @argv: list of all pramas in command line
  * Return: Alaways (0).
 */
 int main(int __attribute__ ((unused)) argc, char *argv[])
