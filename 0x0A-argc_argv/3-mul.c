4a33089ee56b7fd5fb2cc83f699962e6a38a568c
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-15 21:35:14 +0000

    Add mul file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -58,10 +58,10 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
-		return (1);
+		return (0);
 	}
 
 	printf("Error\n");
 
-	return (0);
+	return (1);
 }
