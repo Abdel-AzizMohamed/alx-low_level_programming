a7526412f70b6b8e6f7057b4958cc9792f71a067
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-15 21:25:15 +0000

    Add args file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print out file name
+ * @argc: number of params in command line
+ * @argv: list of all pramas in command line
+ * Return: Alaways (0).
+*/
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc-1);
+
+	return (0);
+}
