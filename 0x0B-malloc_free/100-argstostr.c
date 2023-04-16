a36ca4115265932417554dbe991e162a23d97125
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-16 22:20:20 +0000

    Add argstostr file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * argstostr - concat all the args that
+ * given by command line
+ * @ac: number of args
+ * @av: 2d array contains all args
+ * Return: concated args
+*/
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, len;
+	char *ptr;
+
+	len = 0;
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			len++;
+			j++;
+		}
+		len++;
+	}
+
+	ptr = (char *)malloc(sizeof(char) * len + 1);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			ptr[k] = av[i][j];
+			k++;
+			j++;
+		}
+		ptr[k] = '\n';
+		k++;
+	}
+
+	ptr[k] = '\0';
+
+	return (ptr);
+}
