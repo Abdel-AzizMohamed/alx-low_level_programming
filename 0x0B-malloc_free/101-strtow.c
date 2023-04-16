ecfa0c77f86c8afd4ec7869911597b55de16b379
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-16 23:19:54 +0000

    Add strtow file

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * strtow - split string by spaces
+ * and return array of strings
+ * @str: string
+ * Return: array of strings
+*/
+char **strtow(char *str)
+{
+	int i, j, splits, start, end;
+	char **ptr;
+
+	i = 0;
+
+	if (str == '\0' || str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ')
+			splits++;
+		i++;
+	}
+
+	ptr = (char **)malloc(sizeof(char *) * splits);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+	start = 0;
+	end = 0;
+
+	while (str[end] != '\0')
+	{
+		if (str[end] == ' ')
+		{
+			ptr[j] = (char *)malloc(sizeof(char) * (end - start));
+			while (start < end)
+			{
+				ptr[j][i] = str[start];
+				start++;
+				i++;
+			}
+			ptr[j][i] = '\0';
+			j++;
+			i = 0;
+		}
+		end++;
+	}
+
+	return (ptr);
+}
