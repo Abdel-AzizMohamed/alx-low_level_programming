5df453e0d37453c9be0879d32c63da14a28ef153
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-19 11:54:00 +0000

    Fix int_index function complition error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,15 +8,18 @@
  * @array: the given array for serach
  * @size: size of the array
  * @cmp: search pointer function
+ * Return: if array || cmp || size <= 0 NULL
+ * else if element not found -1
+ * else index of the element
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return;
+		return (-1);
 	else if (size <= 0)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
