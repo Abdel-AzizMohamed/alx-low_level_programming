5340283374c80b620fb6a957623a1a92f3b041a9
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-19 14:50:28 +0000

    Add functions
    added 3-get_op_func.c function
    added 3-op_functions.c function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+
+/**
+ * get_op_func - select op by a given char
+ * @s: the string to select op
+ * Return: if no a vaild op NULL
+ * else pointer to op function
+*/
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op == *s)
+			return (ops[i].f)
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 integers.
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: sub of 2 integers.
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiple 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: multiple of 2 integers.
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divsion 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: disvsion of 2 integers.
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: mod of 2 integers.
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
