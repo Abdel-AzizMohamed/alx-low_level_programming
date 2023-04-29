f37d1f9a60e56b692f2013652a3eddc7e7105524
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-29 16:07:02 +0000

    Add hello hol asm file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,15 @@
-section .data
-    text db "Hello, World!",10
- 
-section .text
-    global _start
- 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
-    syscall
- 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
