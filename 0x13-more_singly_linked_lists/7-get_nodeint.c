2d533596aab093806ed2a546e07a99803d5706f4
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-03 12:38:05 +0000

    Add get_nodeint function file

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get the node at specific index
+ * @head: linked list
+ * @index: the node index
+ * Return: the node at the given index
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	tmp = head;
+
+	for (i = 0; i < index; i++)
+		tmp = tmp->next;
+
+	return (tmp);
+}
