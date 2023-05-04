52f91eca5b2fdad1db6c5f24089091fa69a29f22
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-04 20:05:43 +0000

    Update main header file
    added print_binary prototype
    added base2 prototype
    added strLen prototype

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_binary - prints out converted
+ * decimal to binary
+ * @n: decimal number
+ * Return: Nothing.
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int tmp = 1;
+	int len = 1;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	while (1)
+	{
+		if (tmp * 2 <= n)
+		{
+			tmp *= 2;
+			len++;
+		}
+		else
+			break;
+	}
+	
+	for (; len > 0; len--)
+	{
+		if (base2(len) <= n)
+		{
+			n -= base2(len);
+			printf("1");
+		}
+		else
+			printf("0");
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,8 @@
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int strLen(const char *str);
+unsigned int base2(unsigned int count);
 
 #endif
