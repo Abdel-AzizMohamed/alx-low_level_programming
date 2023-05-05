650e4061143c92475808a1dce58f7331d1fc70e0
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-05 06:43:02 +0000

    Fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,6 +2,36 @@
 
 
 /**
+ * strLen - get the string length
+ * @str: string
+ * Return: string length
+*/
+int strLen(const char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
+/**
+ * base2 - get the 2 power of number
+ * @count: number
+ * Return: 2 power of count
+*/
+unsigned int base2(unsigned int count)
+{
+	unsigned int i, base = 1;
+
+	for (i = 1; i < count; i++)
+		base *= 2;
+
+	return (base);
+}
+
+
+/**
  * binary_to_uint - convert binary to decimal
  * @b: string of binary
  * Return: converted binary
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+
+/**
+ * base2 - get the 2 power of number
+ * @count: number
+ * Return: 2 power of count
+*/
+unsigned int base2(unsigned int count)
+{
+	unsigned int i, base = 1;
+
+	for (i = 1; i < count; i++)
+		base *= 2;
+
+	return (base);
+}
+
+
 /**
  * print_binary - prints out converted
  * decimal to binary
