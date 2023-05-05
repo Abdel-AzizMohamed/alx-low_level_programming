1610f48187161d12d209c6c905b6efec3c967f71
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-05 07:30:19 +0000

    Add get_bit function file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+/**
+ * get_bit - get a bit in given index
+ * @n: decimal number
+ * @index: index of the bit
+ * Return: index of bit || -1 in error
+*/
+ int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int tmp;
+	unsigned int len;
+	int shift = 0, *bits_list, i = 0;
+
+	if (n == 0 && index > 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	
+	for (tmp = n; (tmp >>= 1) > 0; shift++)
+	;
+
+	len = shift + 2;
+	bits_list = malloc(sizeof(int) * len);
+
+	if (index >= len)
+		return (-1);
+
+	for (; shift >= 0; shift--)
+	{
+		if ((n >> shift) & 1)
+			bits_list[i] = 1;
+		else
+			bits_list[i] = 0;
+		i++;
+	}
+	return (bits_list[(len - 2) - index]);
+}
