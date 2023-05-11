7afa74ac1627f3f30d6872748d5acb90beff5629
vagrant <vagrant@linux-vag>
2023-05-11 07:11:25 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	op = open(filename, O_WRONLY | O_CREAT, O_TRUNC, 0600);
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
 
 	if (op == -1 || wr == -1)
