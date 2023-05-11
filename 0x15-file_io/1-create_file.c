6e29105d21545d7926084cb07e3577f83aee10be
vagrant <vagrant@linux-vag>
2023-05-11 07:09:34 +0000

    Add create_file function file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * create_file - creates a file with its content
+ * @filename: given file name
+ * @text_content: text to write to
+ * Return: in success 1 eles -1
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t op, re, wr;
+	size_t len = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		for (; text_content[len];)
+			len++;
+	}
+
+	op = open(filename, O_WRONLY | O_CREAT, O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+	{
+		return (-1);
+	}
+
+	close(op);
+	return (wr);
+}
