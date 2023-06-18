0cc8e5409b12b660f7ed692e966e6d02a4748a2e
vagrant <vagrant@linux-vag20>
2023-06-18 14:57:26 +0000

    Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *fr_node = *h, *sec_node = NULL;
 	unsigned int i;
 
-	if (!*h)
+	if (!*h || !h)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
