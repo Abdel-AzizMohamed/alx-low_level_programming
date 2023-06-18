36c8d331a6caabe14f6d26cadcb42db95cdd812d
vagrant <vagrant@linux-vag20>
2023-06-18 13:32:20 +0000

    Fix task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,11 +14,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
-	*head = new_node;
+	if (*head)
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*head = new_node;	
+	}
 
 	return (new_node);
 }
