350689eb0bb135eeb09f81c26c5461d330610f1d
vagrant <vagrant@linux-vag20>
2023-06-18 13:21:22 +0000

    Fix betty for task 1'dlist_len'

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - give the length of double list
+ * @h: head of list
+ *
+ * Return: Number of nodes
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	int len;
