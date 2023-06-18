420aff74f43a1bf96aa2fbfcec73725270c4ce47
vagrant <vagrant@linux-vag20>
2023-06-18 14:54:41 +0000

    Add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	if (!*new_node)
 		return (NULL);
 
 	new_node->n = n;
