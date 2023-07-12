435e4db38c0f8eb434c3490083e850cf325ec285
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-07-12 10:32:27 +0000

    Add task 1 & update main header

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function that use djb2
+ * @str: given buffer to hash
+ *
+ * Return: hashed buffer
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
