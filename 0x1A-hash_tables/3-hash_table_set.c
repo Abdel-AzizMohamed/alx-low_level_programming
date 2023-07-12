a38bf794c47c4f0cc9bde854db4cca8e59cb6e86
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-07-12 13:18:32 +0000

    Try to fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_ele->value);
 		return (0);
 	}
-	new_ele->next = NULL;
+	new_ele->next = ht->array[index];
 
 	ht->array[index] = new_ele;
 
