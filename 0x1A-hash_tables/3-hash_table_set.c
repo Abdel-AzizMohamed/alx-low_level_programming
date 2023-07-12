b3792d88e4777ff4c55d41e7fa9d91462fb6e90d
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-07-12 11:47:28 +0000

    Add task 3 & update main header

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_set - insert|update element to hash table
+ * @hash: table to insert into
+ * @key: given element key
+ * @value: given element value
+ *
+ * Return: 1 on success else 0
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_ele;
+	unsigned long int index;
+
+	if (!key)
+		return (0);
+
+	new_ele = malloc(sizeof(hash_node_t));
+	if (!new_ele)
+		return (0);
+	new_ele->key = strdup(key);
+	new_ele->value = strdup(value);
+	new_ele->next = NULL;
+
+	index = key_index((unsigned char *) key, ht->size);
+
+	if (!ht->array[index])
+		ht->array[0] = new_ele;
+	else
+		ht->array[index] = new_ele;
+
+	return (1);
+}
