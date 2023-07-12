89038a07ece4981a1ae31cf2b6f32ee51a6d1069
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-07-12 15:50:25 +0000

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: given hash table
+ *
+ * Return: Nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int i;
+	int sep = 0;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (sep == 1)
+			{
+				printf(", ");
+				sep = 0;
+			}
+			printf("'%s': '%s'", temp->key, temp->value);
+			if (temp->next == NULL)
+			{
+				sep = 1;
+				temp = temp->next;
+				continue;
+			}
+			temp = temp->next;
+			printf(", ");
+		}
+	}
+	printf("}\n");
+}
